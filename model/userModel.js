c6bc1fa4965e2840350b11475238f8350bad5597
Patricia Arcanjo <patricia.arcanjo@hotmail.com>
2025-08-23 11:55:58 -0300

    inserindo registro de usuário

--- model/userModel.js
+++ model/userModel.js
@@ -1,20 +1,21 @@
 const bcrypt = require('bcryptjs');
 
 // In-memory user database
-const users = [];  // inicia o banco vazio, mas poderiamos inicializar com alguns usuários
-module.exports = {
-  users
-};
+// const users = [];  // inicia o banco vazio, mas poderiamos inicializar com alguns usuários
+// module.exports = {
+//   users
+// };
 
-// para pré cadastrar os usuários
-//const users = [
-//  { 
-//    username: 'alice', 
-//    password: bcrypt.hashSync('password123', 8),
-//    favorecidos: ['bob'], 
-//    saldo: 10000 
-//  },
-//];  
-//module.exports = {
-//  users
-//};
+//para pré cadastrar os usuários
+const users = [
+ { 
+   username: 'julio', 
+   password: bcrypt.hashSync('123456', 8),
+   favorecidos: ['priscila'], 
+   saldo: 10000 
+ }
+];  
+
+module.exports = {
+ users
+};
